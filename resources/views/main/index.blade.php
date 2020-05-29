ec0699081cd06039e479b79299e3d2620ecb5b9c
admin <55054691+Zen-dz@users.noreply.github.com>
2020-05-29 13:34:17 +0700

    change arrow slide

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -10,12 +10,13 @@
             @endforeach
         </ul>
         <div class="custom-navigation">
-            <a href="#" class="flex-prev text-black-transparant">
-                <i class="fas fa-chevron-left fa-3x"></i>
+            <a href="#" class="flex-prev text-black-transparant img-small">
+                {{-- <i class="fas fa-chevron-left fa-3x"></i> --}}
+            <img src="{{asset('svg/back.svg')}}" alt="">
             </a>
             <div class="custom-controls-container"></div>
-            <a href="#" class="flex-next text-black-transparant">
-                <i class="fas fa-chevron-right fa-3x"></i>
+            <a href="#" class="flex-next text-black-transparant  img-small">
+            <img src="{{asset('svg/next.svg')}}" alt="">
             </a>
         </div>
     </div>
